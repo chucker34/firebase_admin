fb071b58c38527628c3af85f26ac11ab57d70e2b
chucker34 <kawakami34@gmail.com>
2018-08-20 23:20:54 +0900

    database接続

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import * as admin from "firebase-admin";
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY
+  }),
+  databaseURL: process.env.DATABASE_URL
+});
+
+console.log(admin.app().name);
+
+let db = admin.database();
+let ref = db.ref("hoges");
+ref.once("value", (snapshot) => {
+  console.log(snapshot.val());
+});
